5eca5962f8522d51bd837b4dd9b2458c6323c8e3
Saket Verma <saketverma852@gmail.com>
2024-08-20 16:34:13 +0530

    added initial code

---
+++ src/main/java/com/example/logitrack/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.logitrack.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/hello")
+    public String hello() {
+        logger.info("FATAL ERROR");
+        return "Hello Daily Code Buffer!!";
+    }
+}
